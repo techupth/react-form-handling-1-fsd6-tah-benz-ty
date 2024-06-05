4313170ff0f9b2c18266727eb7c8880cd6b84d51
Your Name <you@example.com>
2024-06-05 12:28:56 +0700

    feat: add lable

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState(``);
+  const [inputUsername, setInputUsername] = useState("");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">
+        {username ? `${username}` : "Greeting Message"}
+      </div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          name="username"
+          value={inputUsername}
+          onChange={(e) => {
+            setInputUsername(e.target.value);
+          }}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setUsername(inputUsername);
+          }}
+        >
+          Update text
+        </button>
       </div>
     </div>
   );
