63697e54701c8d2ebfc044b431bd377e5b879eca
Wanchana <s.wanchana41@gmail.com>
2024-06-05 11:45:33 +0700

    feat: add onChange and onClick events via useState from ReactJS, ex.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMassage] = useState("Greeting Message");
+  const [messageInput, setMessageInput] = useState("");
+
+  const handleInputMessage = (event) => {
+    setMessageInput(event.target.value);
+  };
+  const handleUpdateClick = () => {
+    setGreetingMassage(messageInput);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={handleInputMessage}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleUpdateClick}>Update text</button>
       </div>
     </div>
   );
